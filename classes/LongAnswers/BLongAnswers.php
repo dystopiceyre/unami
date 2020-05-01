bd567122ea2361cadcb8cb7a81fe27b270e7c2bf
Olivia Ringhiser <livvy16.olivia@gmail.com>
2020-05-01 00:21:18 -0400

    Basics class constructor and getters, controller

--- classes/LongAnswers/BLongAnswers.php
+++ classes/LongAnswers/BLongAnswers.php
@@ -1,14 +1,228 @@
 <?php
 
-/*
- * Placeholder for Basics training long answers
+/**
+ * Class BLongAnswers
  */
-
 class BLongAnswers
 {
-    public function __construct()
-    {
+    private $_convict;
+    private $_convictText;
+    private $takenBasics;
+    private $takenF2F;
+    private $parentMentalChild;
+    private $childAge;
+    private $givenDiagnosis;
+    private $currentDiagnosis;
+    private $lengthOfIllness;
+    private $publicSchool;
+    private $educationalProgram;
+    private $highSchoolGrad;
+    private $gradDate;
+    private $whyBasicsTeacher;
+    private $childExperiences;
+    private $coteach;
+    private $coteachWith;
+    private $knowWhere;
+    private $teachWhere;
 
+    /**
+     * BLongAnswers constructor.
+     * @param $_convict
+     * @param $_convictText
+     * @param $_takenBasics
+     * @param $_takenF2F
+     * @param $_parentMentalChild
+     * @param $_childAge
+     * @param $_givenDiagnosis
+     * @param $_currentDiagnosis
+     * @param $_lengthOfIllness
+     * @param $_publicSchool
+     * @param $_educationalProgram
+     * @param $_highSchoolGrad
+     * @param $_gradDate
+     * @param $_whyBasicsTeacher
+     * @param $_childExperiences
+     * @param $_coteach
+     * @param $_coteachWith
+     * @param $_knowWhere
+     * @param $_teachWhere
+     */
+    public function __construct($_convict, $_convictText, $_takenBasics, $_takenF2F, $_parentMentalChild, $_childAge, $_givenDiagnosis,
+                                $_currentDiagnosis, $_lengthOfIllness, $_publicSchool, $_educationalProgram,
+                                $_highSchoolGrad, $_gradDate, $_whyBasicsTeacher, $_childExperiences, $_coteach,
+                                $_coteachWith, $_knowWhere, $_teachWhere)
+    {
+        $this->_convict = $_convict;
+        $this->_convictText = $_convictText;
+        $this->takenBasics = $_takenBasics;
+        $this->takenF2F = $_takenF2F;
+        $this->parentMentalChild = $_parentMentalChild;
+        $this->childAge = $_childAge;
+        $this->givenDiagnosis = $_givenDiagnosis;
+        $this->currentDiagnosis = $_currentDiagnosis;
+        $this->lengthOfIllness = $_lengthOfIllness;
+        $this->publicSchool = $_publicSchool;
+        $this->educationalProgram = $_educationalProgram;
+        $this->highSchoolGrad = $_highSchoolGrad;
+        $this->gradDate = $_gradDate;
+        $this->whyBasicsTeacher = $_whyBasicsTeacher;
+        $this->childExperiences = $_childExperiences;
+        $this->coteach = $_coteach;
+        $this->coteachWith = $_coteachWith;
+        $this->knowWhere = $_knowWhere;
+        $this->teachWhere = $_teachWhere;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTakenBasics()
+    {
+        return $this->takenBasics;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTakenF2F()
+    {
+        return $this->takenF2F;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParentMentalChild()
+    {
+        return $this->parentMentalChild;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChildAge()
+    {
+        return $this->childAge;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGivenDiagnosis()
+    {
+        return $this->givenDiagnosis;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrentDiagnosis()
+    {
+        return $this->currentDiagnosis;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPublicSchool()
+    {
+        return $this->publicSchool;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLengthOfIllness()
+    {
+        return $this->lengthOfIllness;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEducationalProgram()
+    {
+        return $this->educationalProgram;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHighSchoolGrad()
+    {
+        return $this->highSchoolGrad;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGradDate()
+    {
+        return $this->gradDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWhyBasicsTeacher()
+    {
+        return $this->whyBasicsTeacher;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChildExperiences()
+    {
+        return $this->childExperiences;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCoteach()
+    {
+        return $this->coteach;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCoteachWith()
+    {
+        return $this->coteachWith;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKnowWhere()
+    {
+        return $this->knowWhere;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTeachWhere()
+    {
+        return $this->teachWhere;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConvict()
+    {
+        return $this->_convict;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getConvictText()
+    {
+        return $this->_convictText;
     }
 
 }
--- controllers/longAnswerControllers/basics/longAnswerController.php
+++ controllers/longAnswerControllers/basics/longAnswerController.php
@@ -1,18 +1,82 @@
 <?php
 global $f3;
 
-if(!empty($_POST))
-{
+if (!empty($POST)) {
     // get data from form
+    $convict = $POST['convict'];
+    $convictText = $POST['convictText'];
+    $takenBasics = $POST['takenBasics'];
+    $takenF2F = $POST['takenF2F'];
+    $parentMentalChild = $POST['parentMentalChild'];
+    $childAge = $POST['childAge'];
+    $givenDiagnosis = $POST['givenDiagnosis'];
+    $currentDiagnosis = $POST['currentDiagnosis'];
+    $lengthOfIllness = $POST['lengthOfIllness'];
+    $publicSchool = $POST['publicSchool'];
+    $educationalProgram = $POST['educationalProgram'];
+    $highSchoolGrad = $POST['highSchoolGrad'];
+    $gradDate = $POST['gradDate'];
+    $whyBasicsTeacher = $POST['whyBasicsTeacher'];
+    $childExperiences = $POST['childExperiences'];
+    $coteach = $POST['coteach'];
+    $coteachWith = $POST['coteachWith'];
+    $knowWhere = $POST['knowWhere'];
+    $teachWhere = $POST['teachWhere'];
+
+
+    if ($convict = 'no') {
+        $convictText = 'N/A';
+    }
+
+    if ($parentMentalChild = 'no') {
+        $childAge = 'N/A';
+        $givenDiagnosis = 'N/A';
+        $currentDiagnosis = 'N/A';
+        $lengthOfIllness = 'N/A';
+    }
+
+    if ($publicSchool = 'no') {
+        $educationalProgram = 'N/A';
+    }
+
+    if ($highSchoolGrad = 'no') {
+        $gradDate = 'N/A';
+    }
+
+    if ($coteach = 'no') {
+        $coteachWith = 'N/A';
+    }
+
+    if ($knowWhere = 'no') {
+        $teachWhere = 'N/A';
+    }
 
     //add data to hive
+    $f3->set('convict', $convict);
+    $f3->set('convictText', $convictText);
+    $f3->set('takenBasics', $takenBasics);
+    $f3->set('takenF2F', $takenF2F);
+    $f3->set('parentMentalChild', $parentMentalChild);
+    $f3->set('childAge', $childAge);
+    $f3->set('givenDiagnosis', $givenDiagnosis);
+    $f3->set('currentDiagnosis', $currentDiagnosis);
+    $f3->set('lengthOfIllness', $lengthOfIllness);
+    $f3->set('publicSchool', $publicSchool);
+    $f3->set('educationalProgram', $educationalProgram);
+    $f3->set('highSchoolGrad', $highSchoolGrad);
+    $f3->set('gradDate', $gradDate);
+    $f3->set('whyBasicsTeacher', $whyBasicsTeacher);
+    $f3->set('childExperiences', $childExperiences);
+    $f3->set('coteach', $coteach);
+    $f3->set('knowWhere', $knowWhere);
+    $f3->set('teachWhere', $teachWhere);
 
-    $_SESSION['LongAnswer'] =  new BLongAnswers();
+    $SESSION['LongAnswer'] = new BLongAnswers($convict, $convictText, $takenBasics, $takenF2F, $parentMentalChild, $childAge,
+        $givenDiagnosis, $currentDiagnosis, $lengthOfIllness, $publicSchool, $educationalProgram, $highSchoolGrad,
+        $gradDate, $whyBasicsTeacher, $childExperiences, $coteach, $coteachWith, $knowWhere, $teachWhere);
 
-    if(validBLongAnswersForm())
-    {
-        if($_POST['goToReview'] == true)
-        {
+    if (validBLongAnswersForm()) {
+        if ($POST['goToReview'] == true) {
             $f3->reroute('/review');
         }
 
@@ -20,7 +84,6 @@ if(!empty($_POST))
     }
 }
 
-if(!isset($_SESSION['LongAnswer']))
-{
-    $_SESSION['LongAnswer'] = new BLongAnswers();
+if (!isset($SESSION['LongAnswer'])) {
+    $SESSION['LongAnswer'] = new BLongAnswers();
 }
