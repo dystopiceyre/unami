abacf8680428ee8956b0991a4471963f09ce8572
Olivia <livvy16.olivia@gmail.com>
2020-11-19 15:00:35 -0800

    Added preferred name column to Excel export

--- classes/Exporter.php
+++ classes/Exporter.php
@@ -11,16 +11,14 @@ class Exporter
         $app_types = $db->getAppTypes();
         $trainingType = '';
 
-        foreach ($app_types as $type)
-        {
-            if($type['app_id'] == $trainingId)
-            {
+        foreach ($app_types as $type) {
+            if ($type['app_id'] == $trainingId) {
                 $trainingType = $type['app_type'];
             }
         }
 
         $timestamp = time();
-        $filename = "$trainingType".'_'."$timestamp.xlsx";
+        $filename = "$trainingType" . '_' . "$timestamp.xlsx";
 
         //create new spreadsheet and select it
         $spreadsheet = new Spreadsheet();
@@ -29,19 +27,20 @@ class Exporter
         $sheet->setCellValue('A1', 'Date Submitted');
         $sheet->setCellValue('B1', 'Application Status');
         $sheet->setCellValue('C1', 'First Name');
-        $sheet->setCellValue('D1', 'Last Name');
-        $sheet->setCellValue('E1', 'Phone #');
-        $sheet->setCellValue('F1', 'Email');
-        $sheet->setCellValue('G1', 'Special Needs');
-        $sheet->setCellValue('H1', 'Service Animal');
-        $sheet->setCellValue('I1', 'Mobility Need');
-        $sheet->setCellValue('J1', 'Needs a Room');
-        $sheet->setCellValue('K1', 'Wants a Single Room');
-        $sheet->setCellValue('L1', 'Days they Need a Room');
-        $sheet->setCellValue('M1', 'Their Gender');
-        $sheet->setCellValue('N1', 'Requested Roommate Gender');
-        $sheet->setCellValue('O1', 'CPAP User');
-        $sheet->setCellValue('P1', "Doesn't mind CPAP using Roommate");
+        $sheet->setCellValue('D1', 'Preferred Name');
+        $sheet->setCellValue('E1', 'Last Name');
+        $sheet->setCellValue('F1', 'Phone #');
+        $sheet->setCellValue('G1', 'Email');
+        $sheet->setCellValue('H1', 'Special Needs');
+        $sheet->setCellValue('I1', 'Service Animal');
+        $sheet->setCellValue('J1', 'Mobility Need');
+        $sheet->setCellValue('K1', 'Needs a Room');
+        $sheet->setCellValue('L1', 'Wants a Single Room');
+        $sheet->setCellValue('M1', 'Days they Need a Room');
+        $sheet->setCellValue('N1', 'Their Gender');
+        $sheet->setCellValue('O1', 'Requested Roommate Gender');
+        $sheet->setCellValue('P1', 'CPAP User');
+        $sheet->setCellValue('Q1', "Doesn't mind CPAP using Roommate");
         $sheet->fromArray($trainingInfo, NULL, 'A2');
 
         $writer = new Xlsx($spreadsheet);
@@ -55,7 +54,7 @@ class Exporter
         if (file_exists($file)) {
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Content-Disposition: attachment; filename="' . basename($file) . '"');
             header('Expires: 0');
             header('Cache-Control: must-revalidate');
             header('Pragma: public');
